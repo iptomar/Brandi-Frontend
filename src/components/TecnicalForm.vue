af04ada2a00787757c4335ed377844b83f59ed04
Alexandre Sequeira <alex-sequeira@hotmail.com>
2018-05-17 19:30:48 +0100

    page 6 frontend

--- src/components/TecnicalForm.vue
+++ src/components/TecnicalForm.vue
@@ -1,11 +1,46 @@
 <template>
-<b-container class="bv-example-row">
-    <b-row class="text-center">
-        <vue-form-generator :schema='schema' :model='model' :options='formOptions'></vue-form-generator>
-    </b-row>
-</b-container>
-</template>
+<b-container class="container">
+    <b-container class="bv-example-row">
+      <!-- Div for form example -->
+      <b-container class="panel panel-default">
+        <b-container class="panel-heading" > Formulário titulo </b-container>
+          <b-container class="panel-body">
+              <vue-form-generator :schema='schema' :model='model' :options='formOptions'></vue-form-generator>
+          </b-container>
+      </b-container>
 
+      <span>**************** START PÁGINA 6 ****************</span>
+      <b-container class="container">
+        <b-container class="panel panel-default">
+          <b-container class="panel-heading" > Intervenções anteriores </b-container>
+            <b-container class="panel-body">
+                <vue-form-generator :schema='schema61' :options='formOptions'></vue-form-generator>
+            </b-container>
+        </b-container>
+      </b-container>
+
+      <b-container class="container">
+        <b-container class="panel panel-default">
+          <b-container class="panel-heading" > Observações | Conclusões </b-container>
+            <b-container class="panel-body">
+                <vue-form-generator :schema='schema62' :options='formOptions'></vue-form-generator>
+            </b-container>
+        </b-container>
+      </b-container>
+
+      <b-container class="container">
+        <b-container class="panel panel-default">
+          <b-container class="panel-heading" > Vontade expressa do proprietário ou do Dono da obra </b-container>
+            <b-container class="panel-body">
+                <vue-form-generator :schema='schema63' :options='formOptions'></vue-form-generator>
+            </b-container>
+        </b-container>
+      </b-container>
+      <span> **************** END PÁGINA 6 ****************</span>          
+
+    </b-container>   
+</b-container> 
+</template>
 <script>
 import Vue from 'vue'
 import VueFormGenerator from 'vue-form-generator'
@@ -170,7 +205,82 @@ export default {
                     }]
                 }]
             },
-
+            // START schema para a página 6 
+            schema61: {
+            fields: [{
+                type: "textArea",
+                label: "Estrutura",
+                model: "estrutura",
+                readonly: false,
+                featured: true,
+                required: true,
+                disabled: false
+            }, {
+                type: "textArea",
+                label: "Superfície",
+                model: "superficie",
+                readonly: false,
+                featured: true,
+                required: true,
+                disabled: false,
+                //validator: VueFormGenerator.validators.string
+            }, {
+                type: "textArea",
+                label: "Elementos Acessórios",
+                model: "elementosacessorios",
+                readonly: false,
+                featured: true,
+                required: true,
+                disabled: false,
+                //validator: VueFormGenerator.validators.string
+            }]
+            },
+            schema62: {
+            fields: [{
+                type: "textArea",
+                model: "conclusao",
+                readonly: false,
+                featured: true,
+                required: true,
+                disabled: false
+            }]
+            },
+            //to add: another check button && seção checkbox on same level
+            schema63: {
+            fields: [{
+                type: "textArea",
+                label: "Tipo de intervenção",
+                model3: "conclusao",
+                readonly: false,
+                featured: true,
+                required: true,
+                disabled: false
+            }, {
+                type: "textArea",
+                label: "Aspectos Específicos",
+                model3: "AspectosEspecificos",
+                readonly: false,
+                featured: true,
+                required: true,
+                disabled: false
+            }, {
+                type: "checkbox",
+                label: "Preservação",
+                model3: "Preservacao",
+                default: true
+            } , {
+                type: "checkbox",
+                label: "Conservação",
+                model: "Conservacao",
+                default: true
+            }, {
+                type: "checkbox",
+                label: "Restauro",
+                model: "Restauro",
+                default: true
+            }]
+            },
+            // END schema para a página 6
 
             formOptions: {
                 validateAfterLoad: true,
@@ -201,4 +311,67 @@ li {
 a {
     color: #42b983;
 }
+html {
+	font-family: Tahoma;
+	font-size: 14px;
+}
+
+body {
+	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+	font-size: 14px;
+	line-height: 1.42857143;
+	color: #333;
+}
+
+pre {
+	overflow: auto;
+}
+	pre .string { color: #885800; }
+	pre .number { color: blue; }
+	pre .boolean { color: magenta; }
+	pre .null { color: red; }
+	pre .key { color: green; }    
+
+h1 {
+	text-align: center;
+	font-size: 36px;
+	margin-top: 20px;
+	margin-bottom: 10px;
+	font-weight: 500;
+}
+
+fieldset {
+	border: 0;
+}
+
+.panel {
+	margin-bottom: 20px;
+	background-color: #fff;
+	border: 1px solid transparent;
+	border-radius: 4px;
+	-webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+	box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+	border-color: #ddd;
+}
+
+.panel-heading {
+	color: #333;
+	background-color: #f5f5f5;
+	border-color: #ddd;
+
+	padding: 10px 15px;
+	border-bottom: 1px solid transparent;
+	border-top-left-radius: 3px;
+	border-top-right-radius: 3px;        
+}
+
+.panel-body {
+	padding: 15px;
+}				
+
+.field-checklist .wrapper {
+	width: 100%;
+}
+</style>
+
 </style>
