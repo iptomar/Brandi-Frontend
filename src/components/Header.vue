06708172a96b7da22fdb306d26536e4a9071f3f0
Alexandre Sequeira <alex-sequeira@hotmail.com>
2018-05-17 21:40:15 +0100

    Adição de css para controlar titulos e subtitulos.sdadasdsadas

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,9 +7,11 @@
                 <b-form-input class="mr-sm-2" type="text" placeholder="Search"></b-form-input>
                 <b-button variant="outline-primary" class="my-2 my-sm-0" type="submit">Search</b-button>
             </b-nav-form>
+            <!-- Mais tarde colocar formulário apenas para quem estiver autenticado -->
+            <b-button variant="outline-primary" class="my-2 my-sm-0" to="/form" type="submit" >Formulário</b-button>
             <b-button variant="outline-primary" class="my-2 my-sm-0" to="/login" type="submit" v-if="!auth.logged && this.$route.path != '/login'">LogIn</b-button>
             <b-nav-item-dropdown right v-if="auth.logged">
-                <!-- Using button-content slot -->
+                <!-- Using button-content slot -->  
                 <template slot="button-content">
                   <em>User</em>
                 </template>
