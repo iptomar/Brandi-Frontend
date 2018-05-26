5d6a7db3cd6ebfb3248eb9daaa20ef3875ffac57
andr000ide <andr1ide@hotmail.com>
2018-05-26 17:21:16 +0100

    Revert "Merge branch 'master' into grupoA-seals2"
    This reverts commit 548ada8dad91ae31c8652ad4449f358621bfdaf5, reversing
    changes made to e1d9d70a9d68c1c71f2e97c126ec62f2eca95294.

--- src/mixins/logged.js
+++ src/mixins/logged.js
@@ -8,32 +8,7 @@ var isLoggedMixin = {
           store.auth.default = false
           store.auth.logged = true
           resolve(true)
-        }
-		if (username === 'gestor' && password === 'gestor') {
-          store.auth.default = false
-          store.auth.logged = true
-          resolve(true)
-        }
-		if (username === 'coordenador' && password === 'coordenador') {
-          store.auth.default = false
-          store.auth.logged = true
-          resolve(true)
-        }
-		if (username === 'diretor' && password === 'diretor') {
-          store.auth.default = false
-          store.auth.logged = true
-          resolve(true)
-        }
-		if (username === 'técnico' && password === 'técnico') {
-          store.auth.default = false
-          store.auth.logged = true
-          resolve(true)
-        }
-		if (username === 'técnicoformação' && password === 'técnicoformação') {
-          store.auth.default = false
-          store.auth.logged = true
-          resolve(true)
-        }else {
+        } else {
           store.auth.logged = false
           resolve(false)
         }
