97770da6604b310120c41914931a3333d7e5e80d
Inês Costa <inescosta17@hotmail.com>
2018-06-13 15:00:13 +0100

    Listar-Analises-css

--- src/components/ListarAnalises.vue
+++ src/components/ListarAnalises.vue
@@ -1,36 +1,40 @@
-<template>
-  <b-container  v-if="auth.logged"  class="bv-example-row">
-  <h1>Lista de Análises</h1>
-  <ul v-if="analises && analises.length">
-    <li v-for="analise of analises" v-bind:key="analise.idAnalise">
-      <table class="tg">
-          <tr>
-            <th class="">Descrição da Análise</th>
-            <th class="">Data da Análise</th>
-            <th class="">Local</th>
-            <th class="">Deslocações (Km)</th>
-            <th class="">Início da Análise</th>
-            <th class="">Fim da Análise</th>
-            <th class="">Outras Despesas</th>
-          </tr>
-          <tr>
-            <td class="">{{deslocacao.descAnalise}}</td>
-            <td class="">{{deslocacao.dataAnalise}}</td>
-            <td class="">{{deslocacao.local}}</td>
-            <td class="">{{deslocacao.km}}</td>
-            <td class="">{{deslocacao.inicAnalise}}</td>
-            <td class="">{{deslocacao.fimAnalise}}</td>
-          </tr>
-      </table>
-  </li>
-</ul>
-
-<ul v-if="errors && errors.length">
-  <li v-for="error of errors" v-bind:key="error.idAnalise">
-    {{error.message}}
-  </li>
-</ul>
-  </b-container>
+ <template>
+    <b-container   class="bv-example-row">
+    <h1>Lista Deslocações</h1>
+    <table class="table table-hover table-dark">
+        <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col">Descrição da Análise</th>
+              <th scope="col">Data da Análise</th>
+              <th scope="col">Local</th>
+              <th scope="col">Deslocações (Km)</th>
+              <th scope="col">Início da Análise</th>
+              <th scope="col">Fim da Análise</th>
+              <th scope="col">Outras Despesas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="deslocacao of deslocacao" v-bind:key="deslocacao.idAnalise">
+              <td>{{deslocacao.idAnalise}}</td>
+              <td>{{deslocacao.descAnalise}}</td>
+              <td>{{deslocacao.dataAnalise}}</td>
+              <td>{{deslocacao.localAnalise}}</td>
+              <td>{{deslocacao.km}}</td>
+              <td>{{deslocacao.inicAnalise}}</td>
+              <td>{{deslocacao.fimAnalise}}</td>
+              <td>{{deslocacao.outrasDespesas}}</td>
+              <td><button type="button" class="btn btn-warning">Editar</button></td>
+              <td><button type="button" class="btn btn-danger">Arquivar</button></td>
+            </tr>
+        </tbody>
+    </table>
+ <ul v-if="errors && errors.length">
+    <li v-for="error of errors" v-bind:key="error.idAnalise">
+      {{error.message}}
+    </li>
+  </ul>
+    </b-container>
 </template>
 
 <script>
@@ -46,16 +50,16 @@ export default {
     data() {
       return {
         auth: store.auth,
-        analises: [],
+        deslocacao: [],
         errors: []
      }
     },
   created() { // informações das deslocações
-    var url = "/listaranalises"
+    var url = "/deslocacoes"
     axios.get(url
     ).then(response => {
-      this.analises = response.data;
-      console.log(this.analises);
+      this.deslocacao = response.data;
+      console.log(this.deslocacao);
     }).catch(e => {
       this.errors.push(e)
   })
@@ -111,7 +115,6 @@ a {
   color: #42b983;
 }
 
-.tg  {border-collapse:collapse;border-spacing:0;}
 td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
 th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;font-weight:bold;}
 
