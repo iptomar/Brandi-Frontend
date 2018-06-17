8c0506abbcc35f6785c30ac4bedfdebbb0c95131
Inês Costa <inescosta17@hotmail.com>
2018-06-17 23:58:17 +0100

    Atualização Listar-Propostas css

--- src/components/ListarPropostas.vue
+++ src/components/ListarPropostas.vue
@@ -1,21 +1,30 @@
 <template>
-  <b-container v-if="auth.logged" class="bv-example-row">
+    <b-container v-if="auth.logged" class="bv-example-row">
     <h1>Lista de Propostas</h1>
-    <ul v-if="propostas && propostas.length">    
-      <li v-for="proposta of propostas" v-bind:key="proposta.idProposta">
-        <p>
-          {{proposta.idproposta}}: {{proposta.descricao}} - {{proposta.dataEnvio}} - {{proposta.aceitacao}}
-          <b-button v-on:click=editar(proposta.idproposta)>Editar</b-button>
-          <b-button v-on:click=apagar(proposta.idproposta)>Apagar</b-button>
-        </p>
-      </li>
-    </ul>
-    <ul v-if="errors && errors.length">
-      <li v-for="error of errors" v-bind:key="error.id">
-        {{error.message}}
-      </li>
-    </ul>
-  </b-container>
+    <table class="table table-hover table-dark">
+            <thead>
+                <tr>
+                <th scope="col">Descrição</th>
+                <th scope="col">Data Envio</th>
+                <th scope="col">Aceitação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="proposta of propostas" v-bind:key="proposta.idProposta">
+                <td>{{proposta.descricao}}</td>
+                <td>{{proposta.dataEnvio}}</td>
+                <td>{{proposta.aceitacao}}</td>
+                <td><button type="button" class="btn btn-warning" v-on:click=editar(proposta.idproposta)>Editar</button></td>
+                <td><button type="button" class="btn btn-danger" v-on:click=arquivar(proposta.idproposta)>Arquivar</button></td>
+            </tr>
+            </tbody>
+        </table> 
+ <ul v-if="errors && errors.length">
+    <li v-for="error of errors" v-bind:key="error.id">
+      {{error.message}}
+    </li>
+  </ul>
+    </b-container>
 </template>
 
 <script>
@@ -76,4 +85,8 @@ li {
 a {
   color: #42b983;
 }
+
+td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;font-weight:bold;}
+
 </style>
